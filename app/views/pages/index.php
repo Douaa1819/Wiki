996591138ba65414260b8bedd531b4c9cee7cbbe
Douaa1819 <douaachemnane@gmail.com>
2024-01-08 11:53:01 +0100

    start frontend

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -10,7 +10,7 @@
 
 <style>
     body {
-        background-color: #DEEFF5; /* zre9 clair */
+        background-color: #FFFFFF; /* zre9 clair */
         margin: 0;
         padding: 0;
         font-family: 'Arial', sans-serif;
